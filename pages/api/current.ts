03db5ab4f41509b1a37a308fb93340845840b99b
DoniyorShodiyevDoni <109288985+Doniyor2555@users.noreply.github.com>
2023-09-09 12:59:32 +0500

    Netflix-Clone

--- pages/api/current.ts
+++ pages/api/current.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 import serverAuth from "@/lib/serverAuth";
 
-export default async function (req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "GET") {
     return res.status(405).end();
   }
